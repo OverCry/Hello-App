798a1bccc16ab7ea279319d4eee2624c2afe60a4
OverCry <54381704+OverCry@users.noreply.github.com>
2020-01-26 18:04:57 -0800

    Complted part b

--- app/src/main/java/com/example/wongy/myapplication/MainActivity.java
+++ app/src/main/java/com/example/wongy/myapplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.wongy.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +20,43 @@ public class MainActivity extends AppCompatActivity {
                 ((TextView)findViewById(R.id.textView)).setTextColor(
                         getResources().getColor(R.color.colorPrimary));
 
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.rootView).setBackgroundColor(
+                        getResources().getColor(R.color.orange));
+            }
+        });
+
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //grab text in editText field and put it as the text
+                String newText = ((EditText)findViewById(R.id.editText)).getText().toString();
+                if (TextUtils.isEmpty(newText)){
+                    ((TextView)findViewById(R.id.textView)).setText("Hello from Wong!");
+                } else {
+                    ((TextView) findViewById(R.id.textView)).setText(newText);
+                }
+            }
+        });
+
+        findViewById(R.id.rootView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //reset text color back to black
+                ((TextView)findViewById(R.id.textView)).setTextColor(
+                        getResources().getColor(R.color.black));
+
+                //reset background color to colorAccent
+                findViewById(R.id.rootView).setBackgroundColor(
+                        getResources().getColor(R.color.colorAccent));
+
+                //reset text to Hello from wong
+                ((TextView)findViewById(R.id.textView)).setText("Hello from Wong!");
             }
         });
     }
